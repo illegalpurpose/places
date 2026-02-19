1d0e738f2156e3d75566f55e695c97cf5f46bb6d
Ilia <iturturika89@gmail.com>
2026-02-19 19:23:00 +0200

    new articles + design

--- src/components/ThemeSwitcher/ThemeSwitcher.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -1,27 +1,45 @@
-import React from 'react'
-import './ThemeSwitcher.css'
+import React from "react";
+import "./ThemeSwitcher.css";
 const ThemeSwitcher = () => {
-    const [currentTheme, setCurrentTheme] = React.useState(localStorage.getItem('theme') || 'light');
+    const [currentTheme, setCurrentTheme] = React.useState(
+        localStorage.getItem("theme") || "light",
+    );
     React.useEffect(() => {
-        setCurrentTheme(localStorage.getItem('theme') || 'light');
-        document.documentElement.setAttribute("data-theme", localStorage.getItem('theme') || 'light');
+        setCurrentTheme(localStorage.getItem("theme") || "light");
+        document.documentElement.setAttribute(
+            "data-theme",
+            localStorage.getItem("theme") || "light",
+        );
     }, []);
 
     const change = () => {
-        if(currentTheme === 'light'){
+        if (currentTheme === "light") {
             document.documentElement.setAttribute("data-theme", "dark");
-            setCurrentTheme('dark');
-            localStorage.setItem("theme", 'dark');
+            setCurrentTheme("dark");
+            localStorage.setItem("theme", "dark");
         } else {
             document.documentElement.setAttribute("data-theme", "light");
-            setCurrentTheme('light');
-            localStorage.setItem("theme", 'light');
+            setCurrentTheme("light");
+            localStorage.setItem("theme", "light");
         }
-    }
-  return (
-    <div className='ThemeSwitcher' style={currentTheme === 'light' ? {backgroundColor: '#1E1E1E'} : {backgroundColor: '#F5F5F5'}} onClick={() => {change()}}>
-    </div>
-  )
-}
+    };
+    return (
+        <div
+            onClick={() => {
+                change();
+            }}
+            className="theme_switcher_background"
+        >
+            <div
+                className="ThemeSwitcher"
+                style={
+                    currentTheme === "light"
+                        ? { backgroundColor: "#F5F5F5" }
+                        : { backgroundColor: "#1E1E1E" }
+                }
+            ></div>
+        </div>
+    );
+};
 
-export default ThemeSwitcher
+export default ThemeSwitcher;
